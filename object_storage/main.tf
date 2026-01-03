82ba40b2eed2af4a32d1b1a83028d9bb1e48a94f
evgenii darkhanov <makaroch817@gmail.com>
2026-01-04 01:34:05 +0300

    upd object_storage/

--- object_storage/main.tf
+++ object_storage/main.tf
@@ -21,6 +21,7 @@ resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
 resource "yandex_storage_bucket" "bucket" {
   access_key            = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key            = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  force_destroy         = true
   bucket                = var.yc_bucket_name
   max_size              = var.yc_bucket_size
   default_storage_class = var.yc_default_storage_class
